bce62da2beb9b95bf48958b2af6c01ef0373e80c
newtalentxp <newtalentxp@126.com>
2020-08-10 07:16:57 +0800

    fix

--- http/gin/middlewares/trace.middleware.go
+++ http/gin/middlewares/trace.middleware.go
@@ -1,35 +1,98 @@
 package middlewares
 
+
+
 import (
+	"net/http"
+	"net/url"
+
 	"github.com/gin-gonic/gin"
-	"github.com/opentracing/opentracing-go"
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 )
 
-type TraceMiddleware struct {
+const defaultComponentName = "net/http"
+
+type mwOptions struct {
+	opNameFunc    func(r *http.Request) string
+	spanObserver  func(span opentracing.Span, r *http.Request)
+	urlTagFunc    func(u *url.URL) string
+	componentName string
 }
 
-func (m *TraceMiddleware) Handler() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		var parentSpan opentracing.Span
-		spCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders,
-			opentracing.HTTPHeadersCarrier(ctx.Request.Header))
-		if err != nil {
-			parentSpan = opentracing.StartSpan(ctx.Request.URL.Path)
-		} else {
-			parentSpan = opentracing.StartSpan(
-				ctx.Request.URL.Path,
-				opentracing.ChildOf(spCtx),
-				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
-			)
-		}
-		defer parentSpan.Finish()
-		
-		ctx.Set("ParentSpanContext", parentSpan.Context())
-		ctx.Next()
+// MWOption controls the behavior of the Middleware.
+type MWOption func(*mwOptions)
+
+// OperationNameFunc returns a MWOption that uses given function f
+// to generate operation name for each server-side span.
+func OperationNameFunc(f func(r *http.Request) string) MWOption {
+	return func(options *mwOptions) {
+		options.opNameFunc = f
 	}
 }
 
-func NewTraceMiddleware() *TraceMiddleware {
-	return &TraceMiddleware{}
+// MWComponentName returns a MWOption that sets the component name
+// for the server-side span.
+func MWComponentName(componentName string) MWOption {
+	return func(options *mwOptions) {
+		options.componentName = componentName
+	}
+}
+
+// MWSpanObserver returns a MWOption that observe the span
+// for the server-side span.
+func MWSpanObserver(f func(span opentracing.Span, r *http.Request)) MWOption {
+	return func(options *mwOptions) {
+		options.spanObserver = f
+	}
+}
+
+// MWURLTagFunc returns a MWOption that uses given function f
+// to set the span's http.url tag. Can be used to change the default
+// http.url tag, eg to redact sensitive information.
+func MWURLTagFunc(f func(u *url.URL) string) MWOption {
+	return func(options *mwOptions) {
+		options.urlTagFunc = f
+	}
+}
+
+// Middleware is a gin native version of the equivalent middleware in:
+//   https://github.com/opentracing-contrib/go-stdlib/
+func Middleware(tr opentracing.Tracer, options ...MWOption) gin.HandlerFunc {
+	opts := mwOptions{
+		opNameFunc: func(r *http.Request) string {
+			return "HTTP " + r.Method
+		},
+		spanObserver: func(span opentracing.Span, r *http.Request) {},
+		urlTagFunc: func(u *url.URL) string {
+			return u.String()
+		},
+	}
+	for _, opt := range options {
+		opt(&opts)
+	}
+
+	return func(c *gin.Context) {
+		carrier := opentracing.HTTPHeadersCarrier(c.Request.Header)
+		ctx, _ := tr.Extract(opentracing.HTTPHeaders, carrier)
+		op := opts.opNameFunc(c.Request)
+		sp := tr.StartSpan(op, ext.RPCServerOption(ctx))
+		ext.HTTPMethod.Set(sp, c.Request.Method)
+		ext.HTTPUrl.Set(sp, opts.urlTagFunc(c.Request.URL))
+		opts.spanObserver(sp, c.Request)
+
+		// set component name, use "net/http" if caller does not specify
+		componentName := opts.componentName
+		if componentName == "" {
+			componentName = defaultComponentName
+		}
+		ext.Component.Set(sp, componentName)
+		c.Request = c.Request.WithContext(
+			opentracing.ContextWithSpan(c.Request.Context(), sp))
+
+		c.Next()
+
+		ext.HTTPStatusCode.Set(sp, uint16(c.Writer.Status()))
+		sp.Finish()
+	}
 }
--- opentracing/jaeger/tracing.provider.go
+++ opentracing/jaeger/tracing.provider.go
@@ -1,98 +1,41 @@
-package middlewares
+package jaeger
 
-
-
-import (
-	"net/http"
-	"net/url"
-
-	"github.com/gin-gonic/gin"
-	opentracing "github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
+import(
+	"github.com/pkg/errors"
+	"github.com/micro/go-micro/v2/config"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-lib/metrics/prometheus"
+	jconfig "github.com/uber/jaeger-client-go/config"
 )
 
-const defaultComponentName = "net/http"
+func NewTracerProvider(config config.Config) (tracer opentracing.Tracer, err error) {
+	serviceName := config.Get("service", "name").String("service")
+	
+	agentAddr := config.Get("jaeger", "agent", "addr").String("localhost:6831")
+	
+	metricsFactory := prometheus.New()
 
-type mwOptions struct {
-	opNameFunc    func(r *http.Request) string
-	spanObserver  func(span opentracing.Span, r *http.Request)
-	urlTagFunc    func(u *url.URL) string
-	componentName string
-}
-
-// MWOption controls the behavior of the Middleware.
-type MWOption func(*mwOptions)
-
-// OperationNameFunc returns a MWOption that uses given function f
-// to generate operation name for each server-side span.
-func OperationNameFunc(f func(r *http.Request) string) MWOption {
-	return func(options *mwOptions) {
-		options.opNameFunc = f
-	}
-}
-
-// MWComponentName returns a MWOption that sets the component name
-// for the server-side span.
-func MWComponentName(componentName string) MWOption {
-	return func(options *mwOptions) {
-		options.componentName = componentName
-	}
-}
-
-// MWSpanObserver returns a MWOption that observe the span
-// for the server-side span.
-func MWSpanObserver(f func(span opentracing.Span, r *http.Request)) MWOption {
-	return func(options *mwOptions) {
-		options.spanObserver = f
-	}
-}
-
-// MWURLTagFunc returns a MWOption that uses given function f
-// to set the span's http.url tag. Can be used to change the default
-// http.url tag, eg to redact sensitive information.
-func MWURLTagFunc(f func(u *url.URL) string) MWOption {
-	return func(options *mwOptions) {
-		options.urlTagFunc = f
-	}
-}
-
-// Middleware is a gin native version of the equivalent middleware in:
-//   https://github.com/opentracing-contrib/go-stdlib/
-func Middleware(tr opentracing.Tracer, options ...MWOption) gin.HandlerFunc {
-	opts := mwOptions{
-		opNameFunc: func(r *http.Request) string {
-			return "HTTP " + r.Method
+	// 根据配置初始化Tracer 返回Closer
+	tracer, _, err = (&jconfig.Configuration{
+		ServiceName: serviceName,
+		Disabled:    false,
+		Sampler: &jconfig.SamplerConfig{
+			Type: jaeger.SamplerTypeConst,
+			// param的值在0到1之间，设置为1则将所有的Operation输出到Reporter
+			Param: 1,
 		},
-		spanObserver: func(span opentracing.Span, r *http.Request) {},
-		urlTagFunc: func(u *url.URL) string {
-			return u.String()
+		Reporter: &jconfig.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: agentAddr,
 		},
+	}).NewTracer(config.Metrics(metricsFactory))
+
+	if err != nil {
+		err = errors.Wrap(err, "create jaeger tracer error")
 	}
-	for _, opt := range options {
-		opt(&opts)
-	}
 
-	return func(c *gin.Context) {
-		carrier := opentracing.HTTPHeadersCarrier(c.Request.Header)
-		ctx, _ := tr.Extract(opentracing.HTTPHeaders, carrier)
-		op := opts.opNameFunc(c.Request)
-		sp := tr.StartSpan(op, ext.RPCServerOption(ctx))
-		ext.HTTPMethod.Set(sp, c.Request.Method)
-		ext.HTTPUrl.Set(sp, opts.urlTagFunc(c.Request.URL))
-		opts.spanObserver(sp, c.Request)
-
-		// set component name, use "net/http" if caller does not specify
-		componentName := opts.componentName
-		if componentName == "" {
-			componentName = defaultComponentName
-		}
-		ext.Component.Set(sp, componentName)
-		c.Request = c.Request.WithContext(
-			opentracing.ContextWithSpan(c.Request.Context(), sp))
-
-		c.Next()
-
-		ext.HTTPStatusCode.Set(sp, uint16(c.Writer.Status()))
-		sp.Finish()
-	}
+	// 设置全局Tracer - 如果不设置将会导致上下文无法生成正确的Span
+	opentracing.SetGlobalTracer(tracer)
+	return
 }
