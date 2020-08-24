3e770ad5b5d6d62d1a09fd1943e6273bd629818f
king <slinpq@outlook.com>
2020-08-24 14:42:33 +0800

    fix

--- opentracing/jaeger/tracing.provider.go
+++ opentracing/jaeger/tracing.provider.go
@@ -1,14 +1,15 @@
 package jaeger
 
-import(
-	"github.com/pkg/errors"
+import (
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2/config"
-	xxxmicro_opentracing "github.com/xxxmicro/base/opentracing"
+	"github.com/micro/go-micro/v2/logger"
 	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
 	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-lib/metrics/prometheus"
 	jconfig "github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-lib/metrics/prometheus"
+	xxxmicro_opentracing "github.com/xxxmicro/base/opentracing"
 )
 
 func NewTracerProvider(c *cli.Context, config config.Config) (tracer opentracing.Tracer, err error) {
@@ -20,9 +21,9 @@ func NewTracerProvider(c *cli.Context, config config.Config) (tracer opentracing
 	if len(serviceName) == 0 {
 		serviceName = "unamed"
 	}
-	
+
 	agentAddr := config.Get("jaeger", "agent", "addr").String("localhost:6831")
-	
+
 	metricsFactory := prometheus.New()
 
 	// 根据配置初始化Tracer 返回Closer
@@ -42,11 +43,12 @@ func NewTracerProvider(c *cli.Context, config config.Config) (tracer opentracing
 
 	if err != nil {
 		err = errors.Wrap(err, "create jaeger tracer error")
+		logger.Error(err)
 	}
 
 	// 设置全局Tracer - 如果不设置将会导致上下文无法生成正确的Span
 	opentracing.SetGlobalTracer(tracer)
 	xxxmicro_opentracing.GlobalTracerWrapper().Wrap(tracer)
-
+	logger.Info("create jaeger tracer success")
 	return
 }
