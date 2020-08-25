8cf63458e3f7be37f74e74949752cd6474168a06
king <slinpq@outlook.com>
2020-08-25 21:54:54 +0800

    fix

--- domain/repository/gorm/base.repository.go
+++ domain/repository/gorm/base.repository.go
@@ -12,7 +12,7 @@ import (
 )
 
 type BaseRepository struct {
-	db *_gorm.DB
+	DB *_gorm.DB
 }
 
 func NewBaseRepository(db *_gorm.DB) repository.BaseRepository {
@@ -20,13 +20,13 @@ func NewBaseRepository(db *_gorm.DB) repository.BaseRepository {
 }
 
 func (r *BaseRepository) Create(c context.Context, m model.Model) error {
-	db := opentracing.SetSpanToGorm(c, r.db)
+	db := opentracing.SetSpanToGorm(c, r.DB)
 
 	return db.Create(m).Error
 }
 
 func (r *BaseRepository) Upsert(c context.Context, m model.Model) (*repository.ChangeInfo, error) {
-	db := opentracing.SetSpanToGorm(c, r.db)
+	db := opentracing.SetSpanToGorm(c, r.DB)
 
 	result := db.Save(m)
 	if result.Error != nil {
@@ -40,10 +40,10 @@ func (r *BaseRepository) Upsert(c context.Context, m model.Model) (*repository.C
 }
 
 func (r *BaseRepository) Update(c context.Context, m model.Model, data interface{}) error {
-	db := opentracing.SetSpanToGorm(c, r.db)
+	db := opentracing.SetSpanToGorm(c, r.DB)
 
 	// 主键保护，如果 m 什么都没设置，这里将会删除表的所有记录
-	scope := r.db.NewScope(m)
+	scope := r.DB.NewScope(m)
 	if scope.PrimaryKeyZero() {
 		return errors.New(fmt.Sprintf("primary key(%s) must be set for update", scope.PrimaryKey()))
 	}
@@ -52,14 +52,14 @@ func (r *BaseRepository) Update(c context.Context, m model.Model, data interface
 }
 
 func (r *BaseRepository) FindOne(c context.Context, m model.Model) error {
-	db := opentracing.SetSpanToGorm(c, r.db)
+	db := opentracing.SetSpanToGorm(c, r.DB)
 
 	return db.Where(m.Unique()).Take(m).Error
 }
 
 func (r *BaseRepository) Delete(c context.Context, m model.Model) error {
 	// 主键保护，如果 m 什么都没设置，这里将会删除表的所有记录
-	ms := r.db.NewScope(m).GetModelStruct()
+	ms := r.DB.NewScope(m).GetModelStruct()
 	for _, pf := range ms.PrimaryFields {
 		value, err := breflect.GetStructField(m, pf.Name)
 		if err != nil {
@@ -71,14 +71,14 @@ func (r *BaseRepository) Delete(c context.Context, m model.Model) error {
 		}
 	}
 
-	return r.db.Delete(m).Error
+	return r.DB.Delete(m).Error
 }
 
 func (r *BaseRepository) Page(c context.Context, m model.Model, query *model.PageQuery, resultPtr interface{}) (total int, pageCount int, err error) {
 	// items := breflect.MakeSlicePtr(m, 0, 0)
-	ms := r.db.NewScope(m).GetModelStruct()
+	ms := r.DB.NewScope(m).GetModelStruct()
 
-	dbHandler := r.db.Model(m)
+	dbHandler := r.DB.Model(m)
 	dbHandler, err = buildQuery(dbHandler, ms, query.Filters)
 	if err != nil {
 		return
@@ -95,9 +95,9 @@ func (r *BaseRepository) Page(c context.Context, m model.Model, query *model.Pag
 }
 
 func (r *BaseRepository) Cursor(c context.Context, query *model.CursorQuery, m model.Model, resultPtr interface{}) (extra *model.CursorExtra, err error) {
-	ms := r.db.NewScope(m).GetModelStruct()
+	ms := r.DB.NewScope(m).GetModelStruct()
 
-	dbHandler := r.db.Model(m)
+	dbHandler := r.DB.Model(m)
 	dbHandler, err = buildQuery(dbHandler, ms, query.Filters)
 	if err != nil {
 		return
